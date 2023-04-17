1aaae972140120d46ce0625fb5ef54a5d9d9fb7d
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-04-17 12:33:10 +0200

    Add project files.

---
+++ Statistics_project/Statistics_project.cpp
@@ -0,0 +1,13 @@
+#include <iostram>
+
+using namespace std;
+
+int main()
+{
+	// Write programs that read a sequence of integer inputs and print:
+	// a. maximum & minimum
+	// b. mean & median & range & standard deviation & variance & mode
+
+
+	return 0;
+}
